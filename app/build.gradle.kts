7e590166d70484936753351d0b439d3295c5185f
yisilan83 <141352987+yisilan83@users.noreply.github.com>
2026-05-01 23:19:15 +0800

    Add networking dependencies (Retrofit, OkHttp, Gson) to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,11 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    // Networking (Retrofit + OkHttp + Gson)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
