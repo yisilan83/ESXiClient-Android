a8ad35f376ce29f420a20ab109729ad482524954
yisilan83 <141352987+yisilan83@users.noreply.github.com>
2026-05-02 02:23:20 +0800

    Bump to version 1.0.9 and fix SOAP login to detect correct protocol (http/https)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.esxiclient.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
     }
 
     buildTypes {
