8e2db6238da722e80bbfc131e21a1cc6c0fd076f
yisilan83 <141352987+yisilan83@users.noreply.github.com>
2026-05-02 11:52:15 +0800

    Bump version to 1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.esxiclient.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.0.11"
     }
 
     buildTypes {
